fc6b9d5a3ffa9c2a12a93a0a396822983d3ec8ee
Pavel Mezencev <mezencev24@mail.ru>
2022-07-26 11:32:12 +0300

    refactor charList render

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -33,35 +33,36 @@ class CharList extends Component {
     this.setState({ loading: false, error: true });
   };
 
+  renderItems = (chars) => {
+    const items = chars.map((char) => {
+      let imgStyles;
+
+      if (
+        char.thumbnail &&
+        char.thumbnail.includes(
+          'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg'
+        )
+      ) {
+        imgStyles = { objectFit: 'fill' };
+      }
+
+      return (
+        <li key={char.id} className='char__item'>
+          <img src={char.thumbnail} alt='abyss' style={imgStyles} />
+          <div className='char__name'>{char.name}</div>
+        </li>
+      );
+    });
+
+    return <ul className='char__grid'>{items}</ul>;
+  };
+
   render() {
     const { loading, error, chars } = this.state;
 
     const spinner = loading ? <Spinner /> : null;
     const errorMessage = error ? <ErrorMessage /> : null;
-    const content =
-      chars.length > 0 ? (
-        <ul className='char__grid'>
-          {chars.map((char) => {
-            let imgStyles;
-
-            if (
-              char.thumbnail &&
-              char.thumbnail.includes(
-                'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg'
-              )
-            ) {
-              imgStyles = { objectFit: 'fill' };
-            }
-
-            return (
-              <li key={char.id} className='char__item'>
-                <img src={char.thumbnail} alt='abyss' style={imgStyles} />
-                <div className='char__name'>{char.name}</div>
-              </li>
-            );
-          })}
-        </ul>
-      ) : null;
+    const content = !(errorMessage || spinner) ? this.renderItems(chars) : null;
 
     return (
       <div className='char__list'>
